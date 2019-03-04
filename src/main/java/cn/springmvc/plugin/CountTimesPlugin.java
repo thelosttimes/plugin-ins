cb9734680691f6e403cd8dc688f25f3850ecc2e9
wuhongsheng <wuhongsheng@9ffenqigo.com>
2019-03-04 10:05:14 +0800

    update

--- src/main/java/cn/springmvc/plugin/CountTimesPlugin.java
+++ src/main/java/cn/springmvc/plugin/CountTimesPlugin.java
@@ -1,27 +1,30 @@
 package cn.springmvc.plugin;
 
 import org.springframework.aop.MethodBeforeAdvice;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Method;
 
 public class CountTimesPlugin implements MethodBeforeAdvice {
 
-    public CountTimesPlugin(){
+    public CountTimesPlugin() {
         System.out.println("===========>>>初始化 CountTimesPlugin");
     }
 
     private int count;
 
-    protected void count(){
-        count ++;
+    protected void count() {
+        count++;
     }
 
     @Override
     public void before(Method method, Object[] args, Object target) throws Throwable {
+        if (method.getDeclaringClass().isAnnotationPresent(Controller.class) || method.getDeclaringClass().isAnnotationPresent(Service.class)) {
             count();
             System.out.println("=======>>> 开始计数");
-            System.out.println(String.format("The method %s invoked times %s",method.getName()+"():",count));
+            System.out.println(String.format("The method %s invoked times %s", method.getName() + "():", count));
+        }
     }
-
 
 }
--- src/main/java/cn/springmvc/plugin/MethodLogPlugin.java
+++ src/main/java/cn/springmvc/plugin/MethodLogPlugin.java
@@ -1,21 +1,24 @@
 package cn.springmvc.plugin;
 
 import org.springframework.aop.MethodBeforeAdvice;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Method;
 
 public class MethodLogPlugin implements MethodBeforeAdvice {
 
-    public MethodLogPlugin(){
+    public MethodLogPlugin() {
         System.out.println("===========>>>初始化 MethodLogPlugin");
     }
 
 
     @Override
     public void before(Method method, Object[] args, Object target) throws Throwable {
+        if (method.getDeclaringClass().isAnnotationPresent(Controller.class) || method.getDeclaringClass().isAnnotationPresent(Service.class)) {
             System.out.println("=======>>> 开始执行");
-            System.out.println(String.format("The calss %s invoked method %s",method.getDeclaringClass().getName()+"():",method.getName()));
+            System.out.println(String.format("The calss %s invoked method %s", method.getDeclaringClass().getName() , method.getName() + "():"));
+        }
     }
-
 
 }
